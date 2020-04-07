3088ee2f89ad42255182557dbe2bb19efe9af1c0
senbinil <senoffical111@gmail.com>
2020-04-07 08:30:55 +0530

    offline files

--- blod.php
+++ blod.php
@@ -46,12 +46,15 @@ while($row=mysqli_fetch_array($query)){
         height:fit-content;
         border-radius: 25px;
     }
+    @font-face{
+      font-family:Montserrat;
+      src: url(fonts/Montserrat-Regular.ttf);
+    }
     </style>
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link href="css/font-awesome.min.css" rel="stylesheet" >
+    <script src="script/jQuery-3.4.1.min.js"></script>
+    <script src="script/bootstrap.bundle.min.js" ></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body>
     
